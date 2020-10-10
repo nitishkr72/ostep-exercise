7941909f23b301249b0110a8bb5dccc4a212155b
nitishkr72 <nitish_tu@outlook.com>
2020-10-10 11:55:16 +0530

    chapter 13 ans intiated

---
+++ 13_AddressSpace/memory-user.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+	printf("enter memory: \n");
+	int m;
+	scanf("%s", &m);
+
+	
+	int arr[sizeof(int)*m];
+	return 0;
+}

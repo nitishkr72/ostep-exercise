9c08c6fe6fb134685b688cd317a4cf38ee2992b0
nitishkr72 <nitish_tu@outlook.com>
2020-11-21 18:45:34 +0530

    previous changes

--- 13_AddressSpace/memory-user.c
+++ 13_AddressSpace/memory-user.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
-	printf("enter memory: \n");
-	int m;
-	scanf("%s", &m);
+#define MEM_SIZE 1024*1024
 
-	
-	int arr[sizeof(int)*m];
+int main(int argc, char ** argv) {
+	if (argc <= 1)
+		exit(0);
+	int mem_val = atoi(argv[1]);
+	char *memUsage;
+	memUsage = (char *)malloc(sizeof(char)*mem_val*MEM_SIZE);
+	for(int i=0;i<mem_val*MEM_SIZE;i++)
+		memUsage[i] = '0';
+	//while(2);
+
 	return 0;
 }
---
+++ 14_MemoryAPI/null.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+	
+	int *ptr;
+	ptr = NULL;
+
+	printf("%d\n", *ptr);
+	return 0;
+}
